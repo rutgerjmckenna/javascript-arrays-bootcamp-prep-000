83477375389a6af2000326ff7c5d190ac8524a93
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-04-10 18:46:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,14 +7,13 @@ function destructivelyAddElementToBeginningOfArray(array, string) {
   array.unshift(string)
   return array
 }
-function addElementToEndOfArray(arrayTwo, stringTwo) {
-  return [arrayTwo,...stringTwo]
+function addElementToEndOfArray(array, string) {
+  return [array,...string]
 }
-function destructivelyAddElementToEndOfArray(arrayTwo, stringTwo) {
-  array.push(stringTwo)
-  return arrayTwo
+function destructivelyAddElementToEndOfArray(array, string) {
+  array.push(string)
+  return array
 }
-// function destructivelyAddElementToEndOfArray() {
 //   var destructiveArray = [1]
 //   destructiveArray.push("foo")
 //   return destructiveArray
